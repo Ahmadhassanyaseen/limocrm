af2b013dd70b3a9310a0d1b69d59f2ed399c2bb5
Hamza Amir <hamzabha543@gmail.com>
2026-01-23 19:02:38 +0500

    changes

--- check.php
+++ check.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>LimoGen Script</title>
 </head>
 <body>
 <div id="limogen-widget"
