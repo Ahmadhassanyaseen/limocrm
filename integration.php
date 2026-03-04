ead07b132f38cdf90e7f7861c4e2e40eb66bc236
Hamza Amir <hamzabha543@gmail.com>
2026-03-04 17:20:30 +0500

    set on team member modules user can create multiple subusers

--- integration.php
+++ integration.php
@@ -26,7 +26,7 @@
   <pre class="crm-code-box" id="widgetCode"><code>&lt;div id="limogen-widget"
      data-user-id="109a9a69-0209-5bfa-4079-696ba7b1cc44"
      data-width="100%"
-     data-height="820"&gt;
+     data-height="820px"&gt;
 &lt;/div&gt;
 
 &lt;script src="https://zabrin.xyz/limogen-widget/widget.js" async&gt;&lt;/script&gt;
